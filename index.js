901791cbf1b640eac52f6311bc71482a1ea04645
Derya Antonelli <derya.antonelli@gmail.com>
2021-07-21 18:37:28 +0100

    Set express routers and data settings

--- index.js
+++ index.js
@@ -1,11 +1,60 @@
+/* eslint-disable no-param-reassign */
+require('dotenv').config();
+const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config();
+const morgan = require('morgan');
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+morgan.token('data', (req) => JSON.stringify(req.body));
+app.use(morgan(':data'));
 
-const PORT = 3003;
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    // eslint-disable-next-line no-underscore-dangle
+    returnedObject.id = returnedObject._id.toString();
+    // eslint-disable-next-line no-underscore-dangle
+    delete returnedObject._id;
+    // eslint-disable-next-line no-underscore-dangle
+    delete returnedObject.__v;
+  },
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+const mongoUrl = process.env.MONGODB_URI;
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body);
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+const { PORT } = process.env;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
