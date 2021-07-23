92ebc1b22a8563073f1844db9633a53fd1e72fe7
Derya Antonelli <derya.antonelli@gmail.com>
2021-07-23 12:09:42 +0100

    Add unit tests for favoriteBlog helper function

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -75,8 +75,104 @@ describe('total likes', () => {
     expect(result).toBe(5);
   });
 
-  test.only('when list has multiple blogs, equals the likes of that', () => {
+  test('when list has multiple blogs, equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithMultipleBlogs);
     expect(result).toBe(36);
+  });
+});
+
+describe('most likes', () => {
+  const listWithDifferentValues = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0,
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0,
+    },
+    {
+      _id: '5a422b891b54a676234d17fa',
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+      __v: 0,
+    },
+  ];
+
+  const listWithSameValues = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0,
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0,
+    },
+    {
+      _id: '5a422b891b54a676234d17fa',
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 12,
+      __v: 0,
+    },
+  ];
+
+  test('when list has multiple blogs, returns which blog has most likes', () => {
+    const result = listHelper.favoriteBlog(listWithDifferentValues);
+    expect(result).toEqual({
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0,
+    });
+  });
+
+  test.only('when list has multiple blogs with many top favourites, returns only one', () => {
+    const result = listHelper.favoriteBlog(listWithSameValues);
+    expect(result).toEqual({
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0,
+    });
   });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,4 +2,11 @@ const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) => blogs.reduce((accu, next) => accu + next.likes, 0);
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) => blogs.reduce((max, next) => {
+  if (next.likes > max.likes) {
+    return next;
+  }
+  return max;
+});
+
+module.exports = { dummy, totalLikes, favoriteBlog };
