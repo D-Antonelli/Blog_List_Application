002cf5c875727bf9aa18de58544aafb64720ede4
Derya Antonelli <derya.antonelli@gmail.com>
2021-07-23 15:07:25 +0100

    Add functions and tests for most likes and most blogs

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -164,7 +164,7 @@ describe('most likes', () => {
     });
   });
 
-  test.only('when list has multiple blogs with many top favourites, returns only one', () => {
+  test('when list has multiple blogs with many top favourites, returns only one', () => {
     const result = listHelper.favoriteBlog(listWithSameValues);
     expect(result).toEqual({
       _id: '5a422b3a1b54a676234d17f9',
@@ -174,5 +174,137 @@ describe('most likes', () => {
       likes: 12,
       __v: 0,
     });
+  });
+});
+
+describe('most blogs', () => {
+  const listWithMultipleBlogs = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0,
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0,
+    },
+    {
+      _id: '5a422b891b54a676234d17fa',
+      title: 'First class tests',
+      author: 'Michael Chan',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+      __v: 0,
+    },
+    {
+      _id: '5a422ba71b54a676234d17fb',
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+      likes: 0,
+      __v: 0,
+    },
+    {
+      _id: '5a422bc61b54a676234d17fc',
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 2,
+      __v: 0,
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  test('returns most blogs', () => {
+    const result = listHelper.mostBlogs(listWithMultipleBlogs);
+    expect(result).toEqual({ author: 'Edsger W. Dijkstra', blogs: 3 });
+  });
+});
+
+describe('mostLikes', () => {
+  const listWithMultipleBlogs = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0,
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0,
+    },
+    {
+      _id: '5a422b891b54a676234d17fa',
+      title: 'First class tests',
+      author: 'Michael Chan',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+      __v: 0,
+    },
+    {
+      _id: '5a422ba71b54a676234d17fb',
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+      likes: 0,
+      __v: 0,
+    },
+    {
+      _id: '5a422bc61b54a676234d17fc',
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 2,
+      __v: 0,
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  test('when multiple blogs are given, returns author with most likes', () => {
+    const result = listHelper.mostLikes(listWithMultipleBlogs);
+    expect(result).toEqual({ author: 'Edsger W. Dijkstra', likes: 22 });
   });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,12 +1,60 @@
 const dummy = (blogs) => 1;
 
-const totalLikes = (blogs) => blogs.reduce((accu, next) => accu + next.likes, 0);
+const totalLikes = (blogs) =>
+  blogs.reduce((accu, current) => accu + current.likes, 0);
 
-const favoriteBlog = (blogs) => blogs.reduce((max, next) => {
-  if (next.likes > max.likes) {
-    return next;
-  }
-  return max;
-});
+const favoriteBlog = (blogs) =>
+  blogs.reduce((max, current) => {
+    if (current.likes > max.likes) {
+      return current;
+    }
+    return max;
+  });
 
-module.exports = { dummy, totalLikes, favoriteBlog };
+const mostBlogs = (blogs) => {
+  const authors = blogs.reduce((array, current) => {
+    const nameMatch = array.find((blog) => blog.author === current.author);
+    if (nameMatch) {
+      nameMatch.blogs += 1;
+    } else {
+      const result = { author: current.author, blogs: 1 };
+      array.push(result);
+    }
+    return array;
+  }, []);
+
+  return authors.reduce((max, current) => {
+    if (current.blogs > max.blogs) {
+      return current;
+    }
+    return max;
+  });
+};
+
+const mostLikes = (blogs) => {
+  const authors = blogs.reduce((array, current) => {
+    const nameMatch = array.find((blog) => blog.author === current.author);
+    if (nameMatch) {
+      nameMatch.likes += current.likes;
+    } else {
+      const result = { author: current.author, likes: current.likes };
+      array.push(result);
+    }
+    return array;
+  }, []);
+
+  return authors.reduce((max, current) => {
+    if (current.likes > max.likes) {
+      return current;
+    }
+    return max;
+  });
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+};
