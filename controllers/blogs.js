15ac12d01768bcfa3b0ed4f3b068b48d735622c1
Derya Antonelli <derya.antonelli@gmail.com>
2021-08-05 13:56:35 +0100

    Implement functionality for delete and update posts

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const logger = require('../utils/logger');
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({});
@@ -16,6 +17,36 @@ blogsRouter.post('/', async (request, response) => {
     const blog = new Blog(post);
     const result = await blog.save();
     response.status(201).json(result);
+  }
+});
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+  const result = await Blog.findByIdAndRemove(id);
+  if (result) {
+    response.status(204).end();
+  } else {
+    response.status(404).json();
+  }
+});
+
+blogsRouter.put('/:id', async (request, response) => {
+  const { body } = request;
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
+
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+      new: true,
+    });
+    response.json(updatedBlog);
+  } catch (err) {
+    logger.info(err);
   }
 });
 
