f448b4fe2118f9b5e4dc3d01c61f30e12739eee4
Derya Antonelli <derya.antonelli@gmail.com>
2021-12-04 17:23:19 +0000

    adds login controller

---
+++ controllers/login.js
@@ -0,0 +1,33 @@
+const loginRouter = require('express').Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+loginRouter.post('/', async (request, response) => {
+  const { body } = request;
+
+  const user = await User.findById(body.username);
+  const passwordCorrect =
+    user === null
+      ? false
+      : await bcrypt.compare(body.password, user.passwordHash);
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password',
+    });
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user.id,
+  };
+
+  const token = jwt.sign(userForToken, process.env.SECRET);
+
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name });
+});
+
+module.exports = loginRouter;
