c9182148e264648e2fc024a720e43e329409184d
Derya Antonelli <derya.antonelli@gmail.com>
2021-12-04 15:08:13 +0000

    implements join query

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,19 +1,23 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 const logger = require('../utils/logger');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user');
   response.json(blogs);
 });
 
 blogsRouter.post('/', async (request, response) => {
   const { body } = request;
 
+  const user = await User.findOne({});
+
   if (!body.title && !body.url) {
     response.status(400).json();
   } else {
     const post = body.likes ? body : { ...body, likes: 0 };
+    post.user = user.id;
     const blog = new Blog(post);
     const result = await blog.save();
     response.status(201).json(result);
